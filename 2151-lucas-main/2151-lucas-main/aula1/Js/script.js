644d63ea7298712720ce083354f6f3c970994b51
Luc4s G4br13l P3r31r4 d4 Cunh4 <91632301+lucas0456@users.noreply.github.com>
2022-04-26 07:32:25 -0300

    ss
    ss

--- 2151-lucas-main/2151-lucas-main/aula1/Js/script.js
+++
@@ -1,5 +0,0 @@
-function trocar(caminho, db, lg){
-    document.getElementById('imagem_principal').src= caminho; 
-    document.getElementById('titulo').value= db;
-    document.getElementById('legenda').value= lg;
-}

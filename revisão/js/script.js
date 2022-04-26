6bab2d98e6aa61978c29282e5e10effd1be1f34c
Luc4s G4br13l P3r31r4 d4 Cunh4 <91632301+lucas0456@users.noreply.github.com>
2022-04-26 14:41:17 -0300

    s
    s

--- revisão/js/script.js
+++ revisão/js/script.js
@@ -1,4 +1,5 @@
 function cal() {
+
     //entrada
     let horario = document.form.horario.value;
    
@@ -21,7 +22,6 @@ function cal() {
     document.getElementById('fest5').checked = false;
 
 }
-
      function op(valor,marcado) {
             //entrada
             total = document.getElementById('total').value;
@@ -37,4 +37,52 @@ function cal() {
         }
         //saida
         document.getElementById('total').value = total;
+    }
+    function trocatema(){
+        //entrada
+        tema = document.getElementById('tema').value;
+
+        //processamento
+        if(tema == "mario"){
+            titulo = "Super Mario Bros";
+            newFundo = "img/mario.jpg";
+            newFoto = "img/mario.png";
+            cor = "red";
+            altura = "85%";
+            right = "0%";
+            bottom = "-5";
+        } else if(tema == "sofia"){
+            titulo = "Princesa Sofia";
+            newFundo = "img/sofia.jpg";
+            newFoto = "img/sofia.png";
+            cor = "purple";
+            altura = "90%";
+            right = "5%";
+            bottom = "-5%";
+        } else if(tema == "masks"){
+            titulo = "Pj masks";
+            newFundo = "img/pjmasks.jpg";
+            newFoto = "img/pjmasks.png";
+            cor = "blue";
+            altura = "85%";
+            right = "-2%";
+            bottom = "0%";
+        } else{
+            titulo = "Tema das festas";
+            newFundo = "";
+            newFoto = "";
+            cor = "black";
+            altura = "100";
+            right = "0%";
+            bottom = "0";
+        }
+
+        //saida
+        document.getElementById('titulo').value = titulo
+        document.body.style.backgroundImage = "url(" + newFundo + ")";
+        document.getElementById('foto').src = newFoto;
+        document.getElementById('titulo').style.color = cor;
+        document.body.style.color = cor;
+
+        document
     }
